dd32cd010cc288a0de7a66349526483ce511fbd7
gscd221 <156783782+gscd221@users.noreply.github.com>
2024-07-03 10:47:59 +0900

    2024/07/03: 수도권 전철 조건문 완료 밎 노트북 추가
    다음 과제:
    Turtle을 사용하여 각 노선의 노선도 그리기
    역 정보 일부 추가하기

--- metroproject.py
+++ metroproject.py
@@ -1,9 +1,12 @@
 from tkinter import *
+from tkinter import ttk
+win=Tk()
 
+places = ["수도권","부산","대구","대전","광주"]
 sgi = ["1호선", "2호선", "3호선", "4호선", "5호선", "6호선", "7호선", "8호선", "9호선", "인천1호선", "인천2호선", "신분당선", "경의중앙선", "경춘선", "수인분당선", "공항철도", "의정부경전철", "용인에버라인", "경강선", "우이신설선", "서해선", "김포골드라인", "신림선", "GTX-A"]
-selineone = ["연천","전곡","청산","소요산","동두천","보산","동두천중앙","지행","덕정","덕계","양주","녹양","가능","의정부","회룡","망월사","도봉산","도봉","방학","창동","녹천","월계","광운대","석계","신이문","외대앞","회기","청량리(서울시립대입구)","제기동(한국건강관리협회)","신설동","동묘앞","동대문","종로5가(삼양그룹)","종로3가","종각","시청","서울","남영","용산","노량진","대방","신길","영등포","신도림","구로","구일","개봉","오류동","온수","역곡","소사","부천","중동","송내","부개","부평","백운","동암","간석","주안","도화","제물포","도원","동인천","인천","가산디지털단지","독산","금천구청","광명","석수","관악","안양","명학","금정","군포","당정","의왕","성균관대","화서","수원","세류","병점","서동탄","세마","오산대","오산","진위","송탄","서정리","평택지제","평택","성환","직산","두정","천안","봉명","쌍용(나사렛대)","아산","탕정","배방","온양온천","신창(순천향대)"]
-selinetwo = ["시청","을지로입구(하나은행)","을지로3가(신한카드)","을지로4가(BC카드)","동대문역사문화공원(DDP)","신당","상왕십리","왕십리(성동구청)","한양대","뚝섬","성수","건대입구","구의(광진구청)","강변(동서울터미널)","잠실나루(수협중앙회공제보험)","잠실(송파구청)","잠실새내","종합운동장","삼성(무역센터)","선릉(에큐온저축은행)","역삼(센터필드)","강남","교대(법원.검찰청)","서초","방배(백석예술대)","사당","낙성대(강감찬)","서울대입구(관악구청)","봉천","신림","신대방","구로디지털단지(원광디지털대)","대림(구로구청)","신도림","문래(김안과병원)","영등포구청","당산","합정(세아타워)","홍대입구","신촌","이대","아현","충정로(경기대입구)","용답","신답","용두(동대문구청)","신설동","도림천","양천구청","신정네거리","까치산"]
-selinethree = ["대화","주엽","정발산","마두","백석","대곡","화정","원당","원흥","삼송","지축","구파발(은평성모병원)","연신내","불광","녹번","홍제","무악재","독립문","경복궁(정부서울청사)","안국(현대건설)","종로3가","을지로3가(신한카드)","충무로","동대입구","약수","금호","옥수","압구정","신사","잠원","고속터미널","교대(법원.검찰청)","남부터미널","양재(서초구청)","매봉","도곡","대치","학여울","대청(서울주택도시공사)","일원","수서","가락시장","경찰병원","오금"]
+selineone = ["연천","전곡","청산","소요산","동두천","보산","동두천중앙","지행","덕정","덕계","양주","녹양","가능","의정부","회룡","망월사","도봉산","도봉","방학","창동","녹천","월계","광운대","석계","신이문","외대앞","회기","청량리","제기동","신설동","동묘앞","동대문","종로5가","종로3가","종각","시청","서울","남영","용산","노량진","대방","신길","영등포","신도림","구로","구일","개봉","오류동","온수","역곡","소사","부천","중동","송내","부개","부평","백운","동암","간석","주안","도화","제물포","도원","동인천","인천","가산디지털단지","독산","금천구청","광명","석수","관악","안양","명학","금정","군포","당정","의왕","성균관대","화서","수원","세류","병점","서동탄","세마","오산대","오산","진위","송탄","서정리","평택지제","평택","성환","직산","두정","천안","봉명","쌍용","아산","탕정","배방","온양온천","신창"]
+selinetwo = ["시청","을지로입구","을지로3가","을지로4가","동대문역사문화공원","신당","상왕십리","왕십리","한양대","뚝섬","성수","건대입구","구의","강변","잠실나루","잠실","잠실새내","종합운동장","삼성","선릉","역삼","강남","교대","서초","방배","사당","낙성대","서울대입구","봉천","신림","신대방","구로디지털단지","대림","신도림","문래","영등포구청","당산","합정","홍대입구","신촌","이대","아현","충정로","용답","신답","용두","신설동","도림천","양천구청","신정네거리","까치산"]
+selinethree = ["대화","주엽","정발산","마두","백석","대곡","화정","원당","원흥","삼송","지축","구파발","연신내","불광","녹번","홍제","무악재","독립문","경복궁","안국(현대건설)","종로3가","을지로3가(신한카드)","충무로","동대입구","약수","금호","옥수","압구정","신사","잠원","고속터미널","교대(법원.검찰청)","남부터미널","양재(서초구청)","매봉","도곡","대치","학여울","대청(서울주택도시공사)","일원","수서","가락시장","경찰병원","오금"]
 selinefour = ["진접","오남","별내별가람","당고개","상계","노원","창동","쌍문","수유(강북구청)","미아(서울사이버대학)","미아사거리","길음","성신여대입구(돈암)","한성대입구(삼선교)","혜화(서울대학교병원)","동대문","동대문역사문화공원(DDP)","충무로","명동(우리금융타운)","회현(남대문시장)","서울","숙대입구","삼각지","신용산(아모레퍼시픽)","이촌(국립중앙박물관)","동작(현충원)","이수(총신대입구)","사당","남태령","선바위","경마공원","대공원","과천","정부과천청사","인덕원","평촌","범계","금정","산본","수리산","대야미","반월","상록수","한대앞","중앙","고잔","초지","안산","신길온천","정왕","오이도"]
 selinefive = ["방화","개화산","김포공항","송정","마곡(홈앤쇼핑)","발산(에스앤유서울병원)","우장산","화곡","까치산","신정(은행정)","목동","오목교(목동운동장앞)","양평","영등포구청","영등포시장","신길","여의도(신한투자증권)","여의나루","마포","공덕","애오개","충정로(경기대입구)","서대문(강북삼성병원)","광화문(세종문화회관)","종로3가(탑골공원)","을지로4가(BC카드)","동대문역사문화공원(DDP)","청구","신금호","행당","왕십리(성동구청)","마장","답십리","장한평","군자(능동)","아차산(어린이대공원후문)","광나루(장신대)","천호(풍납토성)","강동(강동성심병원)","길동","굽은다리(강동구민회관)","명일","고덕(강동경희대병원)","상일동","강일","하남풍산","하남시청(덕풍.신장)","하남검단산","둔촌동","올림픽공원(한국체대)","방이","오금","개롱","거여","마천"]
 selinesix = ["역촌","불광","독바위","연신내","구산","응암","새절(신사)","증산(명지대앞)","디지털미디어시티","월드컵경기장(성산)","마포구청","망원","합정(세아타워)","상수","광흥창(서강)","대흥(서강대앞)","공덕","효창공원앞","삼각지","녹사평(용산구청)","이태원","한강진","버티고개","약수","청구","신당","동묘앞","창신","보문","안암(고대병원앞)","고려대(종암)","월곡(동덕여대)","상월곡(한국과학기술연구원)","돌곶이","석계","태릉입구","화랑대(서울여대입구)","봉화산(서울의료원)","신내"]
@@ -27,21 +30,51 @@ sinlimline = ["샛강","대방(성애병원)","서울지방병무청","보라매
 gtxa = ["수서","성남","구성","동탄"]
 pk = ["1호선","2호선","3호선","4호선","부산-김해경전철","동해선"]
 pusanline1 = ["다대포해수욕장","다대포항","낫개","신장림","장림","동매","신평","하단","당리","사하","괴정","대티","서대신","동대신","토성","자갈치","남포","중앙","부산","초량","부산진","좌천","범일","범내골","서면","부전","양정","시청","연산","교대","동래","명륜","온천장","부산대","장전","구서","두실","남산","범어사","노포"]
-pusanline2 = ["장산","중동","해운대","동백","BEXCO","센텀시티","민락","수영","광안","금련산"]
+pusanline2 = ["장산","중동","해운대","동백","BEXCO","센텀시티","민락","수영","광안","금련산","남천","경성대·부경대", "대연", "못골", "지게골", "문현", "국제금융센터·부산은행", "전포", "서면", "부암","가야", "동의대", "개금", "냉정", "주례", "감전", "사상", "덕포", "모덕", "모라", "구남", "구명","덕천", "수정", "화명", "율리", "동원", "금곡", "호포", "증산", "부산대양산캠퍼스", "남양산", "양산"]
 tk = ["1호선","2호선","3호선"]
 dj = ["1호선"]
 gj = ["1호선"]
 
-listbox = Listbox()
-listbox2 = Listbox()
-listbox3=Listbox()
-listbox4=Listbox()
+notebook=ttk.Notebook(win,width=300,height=900)
+notebook.pack()
+fr=Frame(win)
+fr1=Frame(win)
+fr2=Frame(win)
+notebook.add(fr,text="리스트 선택")
+notebook.add(fr1,text="노선도")
+
+listbox = Listbox(fr)
+listbox2 = Listbox(fr)
+listbox3=Listbox(fr)
+listbox4=Listbox(fr)
+la=Label(fr,text="지역 선택")
+la1=Label(fr,text="노선 선택")
+la2=Label(fr,text="역 선택")
+la3=Label(fr,text="역 정보")
 
 def sel(self):
     print(listbox.curselection())
     listbox2.delete(0,END)
-    for item in sgi:
-        listbox2.insert(END, item)
+    if(listbox.curselection()[0]==0):
+        listbox2.delete(0,END)
+        for item in sgi:
+            listbox2.insert(END, item)
+    if(listbox.curselection()[0]==1):
+        listbox2.delete(0,END)
+        for item in pk:
+            listbox2.insert(END, item)
+    if(listbox.curselection()[0]==2):
+        listbox2.delete(0,END)
+        for item in tk:
+            listbox2.insert(END, item)
+    if(listbox.curselection()[0]==3):
+        listbox2.delete(0,END)
+        for item in dj:
+            listbox2.insert(END, item)
+    if(listbox.curselection()[0]==4):
+        listbox2.delete(0,END)
+        for item in gj:
+            listbox2.insert(END, item)
 
 def listwo(self):
     print(listbox.curselection())
@@ -57,13 +90,103 @@ def listwo(self):
         listbox3.delete(0,END)
         for item in selinethree:
             listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==3):
+        listbox3.delete(0,END)
+        for item in selinefour:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==4):
+        listbox3.delete(0,END)
+        for item in selinefive:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==5):
+        listbox3.delete(0,END)
+        for item in selinesix:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==6):
+        listbox3.delete(0,END)
+        for item in selineseven:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==7):
+        listbox3.delete(0,END)
+        for item in selineeight:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==8):
+        listbox3.delete(0,END)
+        for item in selinenine:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==9):
+        listbox3.delete(0,END)
+        for item in icnlineone:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==10):
+        listbox3.delete(0,END)
+        for item in icnlinetwo:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==11):
+        listbox3.delete(0,END)
+        for item in newbundangline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==12):
+        listbox3.delete(0,END)
+        for item in gjline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==13):
+        listbox3.delete(0,END)
+        for item in gyungchunline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==14):
+        listbox3.delete(0,END)
+        for item in sbline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==15):
+        listbox3.delete(0,END)
+        for item in arex:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==16):
+        listbox3.delete(0,END)
+        for item in ujblrt:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==17):
+        listbox3.delete(0,END)
+        for item in everline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==18):
+        listbox3.delete(0,END)
+        for item in gyunggangline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==19):
+        listbox3.delete(0,END)
+        for item in uilrt:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==20):
+        listbox3.delete(0,END)
+        for item in suheline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==21):
+        listbox3.delete(0,END)
+        for item in goldline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==22):
+        listbox3.delete(0,END)
+        for item in sinlimline:
+            listbox3.insert(END, item)
+    if(listbox2.curselection()[0]==23):
+        listbox3.delete(0,END)
+        for item in gtxa:
+            listbox3.insert(END, item)
 
 listbox2.bind("<ButtonRelease-1>",listwo)
 listbox.bind("<ButtonRelease-1>", sel)
-listbox.insert(0, "수도권")
-
+for item in places:
+    listbox.insert(END,item)
+ 
+la.pack()
 listbox.pack()
+la1.pack()
 listbox2.pack()
+la2.pack()
 listbox3.pack()
+la3.pack()
+listbox4.pack()
 
 mainloop()
